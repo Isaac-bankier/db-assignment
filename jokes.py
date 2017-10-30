d84decb5da5fd41501f7eeda626a17049097a8a7
Isaac Bankier <isaacbankier@gmail.com>
2017-10-30 21:09:12 +1100

    Start of the dadabse.

---
+++ jokes.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+import time
+import random
+
+user = {'User-agent': 'DadJokeFinder v1.1'}
+r = requests.get('http://www.reddit.com/r/dadjokes/top/.json', headers=user)
+data = r.json()
+
+title=""
+punchline=""
+data=data['data']['children']
+
+for joke in data:
+	title=joke['data']['title']
+	punchline=joke['data']['selftext']
+	print("------------------------------------")
+	print(title)
+	print("\n")
+	print(punchline)
+	print("------------------------------------")
