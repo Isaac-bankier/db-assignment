648fdcd7c4f231b5dcded7dbaf85d452cb92b35b
Isaac Bankier <isaacbankier@gmail.com>
2017-10-30 23:10:36 +1100

    Defined error handling.

--- jokes.py
+++ jokes.py
@@ -16,40 +16,41 @@ def jokeHash(title, punchline, date, user):
 	hasher = hashlib.sha512()
 	hasher.update(title.encode('ascii', 'ignore') + punchline.encode('ascii', 'ignore') + str(date).encode('ascii', 'ignore') + user.encode('ascii', 'ignore'))
 	return 	hasher.hexdigest()
+try:
+	for count in range(amount):
+		r = requests.get('http://www.reddit.com/r/dadjokes/top/.json?sort=top&t=all&after='+nextRequest, headers=userAgent)
+		rawData = r.json()
+		data=rawData['data']['children']
+		nextRequest=rawData['data']['after']
 
-for count in range(amount):
-	r = requests.get('http://www.reddit.com/r/dadjokes/top/.json?sort=top&t=all&after='+nextRequest, headers=userAgent)
-	rawData = r.json()
-	data=rawData['data']['children']
-	nextRequest=rawData['data']['after']
+		for joke in data:
+			title=joke['data']['title']
+			punchline=joke['data']['selftext']
+			dataBaseId+=1
+			edited=joke['data']['edited']
+			over_18=joke['data']['over_18']
+			user=joke['data']['author']
+			postTime=joke['data']['created_utc']
+			upvotes=joke['data']['score']
+			archived=joke['data']['archived']
+			comments=joke['data']['num_comments']
+			hash = jokeHash(title, punchline, postTime, user)
 
-	for joke in data:
-		title=joke['data']['title']
-		punchline=joke['data']['selftext']
-		dataBaseId+=1
-		edited=joke['data']['edited']
-		over_18=joke['data']['over_18']
-		user=joke['data']['author']
-		postTime=joke['data']['created_utc']
-		upvotes=joke['data']['score']
-		archived=joke['data']['archived']
-		comments=joke['data']['num_comments']
-		hash = jokeHash(title, punchline, postTime, user)
+			databasePush = [dataBaseId, title, punchline, postTime, upvotes, user, hash]
+			database2Push = [dataBaseId, upvotes, edited, over_18, archived, comments, hash]
+			dadaBase.append(databasePush)
+			dadaBaseReact.append(database2Push)
 
-		databasePush = [dataBaseId, title, punchline, postTime, upvotes, user, hash]
-		database2Push = [dataBaseId, upvotes, edited, over_18, archived, comments, hash]
-		dadaBase.append(databasePush)
-		dadaBaseReact.append(database2Push)
+		time.sleep(2)
 
-	time.sleep(2)
+finally:
+	writer = csv.writer(open("/home/isaac/Documents/code/database-assignment-year-9/dadaBase.csv", "w"))
+	for row in dadaBase:
+		writer.writerow(row)
 
-writer = csv.writer(open("/home/isaac/Documents/code/database-assignment-year-9/dadaBase.csv", "rw"))
-for row in dadaBase:
-	writer.writerow(row)
+	writer = csv.writer(open("/home/isaac/Documents/code/database-assignment-year-9/dadaBaseReact.csv", "w"))
+	for row in dadaBaseReact:
+		writer.writerow(row)
 
-writer = csv.writer(open("/home/isaac/Documents/code/database-assignment-year-9/dadaBaseReact.csv", "rw"))
-for row in dadaBaseReact:
-	writer.writerow(row)
-
-print("Wrote data on "+str(len(dadaBase)+" dad jokes."))
-print("Done.")
+	print("Wrote data on "+str(len(dadaBase)-1)+" dad jokes.")
+	print("Done.")
